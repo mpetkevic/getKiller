66aeb00c53f82be94298aca22f5ecc740cffadd8
mpetkevic <miroslav.petkevic@gmail.com>
2022-05-27 10:42:36 +0300

    Improvements #2

--- task-3.js
+++ task-3.js
@@ -1,6 +1,14 @@
 function getKiller(suspectInfo, deadPeople) {
     for (const killer in suspectInfo) {
         const people = Object.values(suspectInfo[killer]);
+        
+        // на будушее
+        // здесь можешь поставить чек, если массив пустой
+        // дальше код остановить для перфонманса
+        // чтоб не грузить оперативку и проц
+        // если обьект будет больше и массивы длинее
+
+        // например if(people.length === 0) return
         const containsAllDeadPeople = deadPeople.every(element => {
             return people.includes(element);
         });
